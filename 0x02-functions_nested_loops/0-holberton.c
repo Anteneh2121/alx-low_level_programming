498ec793027b38c883b82de34afda7edbfd50bb1
Anteneh <antukassaw1@gmail.com>
2021-08-31 06:08:18 -0700

    holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,12 +1,19 @@
 #include "main.h"
+/**
+ * main-print Holberton
+ *return always (0)
+ */
 
 int main(void)
+
 {
-  char *c = "Holberton";
-  int i;
-  for(i = 0; i < 10; i++){
-    _putchar(c[i]);
-  }
-  _putchar('\n');
-  return (0);
+
+char *c = "Holberton";
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
 }
