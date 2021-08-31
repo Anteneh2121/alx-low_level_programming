16d7b57d88840ee135a9911c193f7b85826e6235
Anteneh <antukassaw1@gmail.com>
2021-08-31 07:37:58 -0700

    commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,13 +6,14 @@
  */
 int main(void)
 {
-char *sh = "Holberton";
-
-while (*sh)
-{
-_putchar(*sh);
-sh++;
-}
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');_putchar('o');
+_putchar('n');
 _putchar('\n');
 
 return (0);
