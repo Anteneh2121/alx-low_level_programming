e884193479d2b5f547834d44b642bc893ffe43a4
Anteneh <antukassaw1@gmail.com>
2021-09-01 03:42:34 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,12 @@
+/**
+ * main - prints to string
+ * Description: Prints positive, negative or zero
+ * Return: 0
+ */
+#include <stdio.h>
+int main(void)
+{
+printf("_putchar");
+putchar ('\n');
+return (0);
+}
