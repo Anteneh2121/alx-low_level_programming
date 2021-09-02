0c810095720cb18a9fe82e5504db551d81368b2c
Anteneh <antukassaw1@gmail.com>
2021-09-02 07:15:34 -0700

    commit

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -6,10 +7,10 @@
 
 int main(void)
 {
-  
-int i;
+  int i;
 
-i = 98;
-positive_or_negative(i);
-return (0);
+  i = 98;
+  positive_or_negative(i);
+
+  return (0);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -6,10 +7,10 @@
 
 int main(void)
 {
-  
 int i;
 
-i = 98;
+i = 0;
 positive_or_negative(i);
+
 return (0);
 }
