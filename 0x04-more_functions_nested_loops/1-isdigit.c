ff774bbe2f838ef177d99f2efe3bfb365bd1f66a
Anteneh <antukassaw1@gmail.com>
2021-09-02 04:58:41 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,18 @@
-#include <stdio.h>
 #include "main.h"
-#include "holberton.h"
 /**
- * _isupper - function that checks for uppercase character.
- * @c: the int for the paramaters of my function
- * Return: 0
+ * _isdigit - checks for a digit
+ * @c: the parameter to print
+ *
+ * Return: 1 or 0
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
-  if (c >= 65 && c <= 90)
+  if (c >= 48 && c <= 57)
     {
       return (1);
     }
   else
-    return (0);
+    {
+      return (0);
+    }
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,5 +3,5 @@
 #include <stdio.h>
 
 int _isupper(int c);
-
+int _isdigit(int c);
 #endif /* MAIN_H */
