be0dce4e67a9b2275a134a9146f0c6a69a71f067
Anteneh <antukassaw1@gmail.com>
2021-09-02 03:38:41 -0700

    holberton

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,7 +9,7 @@ int main(void)
 int i;
 
 i = 0;
-positive_or_negative(i);
+ void positive_or_negative(int i);
 
 return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include "holberton.h"
 #include <stdio.h>
 
 void positive_or_negative(int i);
