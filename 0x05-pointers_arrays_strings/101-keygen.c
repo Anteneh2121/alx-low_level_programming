2bd31b91e8f115a29f4fdbc1a40979acfc230d31
Anteneh <antukassaw1@gmail.com>
2021-09-06 02:13:58 -0700

    commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,28 +1,36 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - check the code for Holberton School students.
- * 
- * Return: Always 0.
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-  char seed[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  char password[22];
-  int i, n, passw, complem;
+int i, sum, n;
+int pass[100];
 
-  srand(time(0));
+sum = 0;
 
-  for (i = 0; passw < 2772; i++)
-    {
-      n = rand() % 10;
-      password[i] = seed[n];
-      passw += password[i];
-    }
-  complem = 2772 - passw;
-  password[i] = complem;
-  printf("%s\n",  password);
-  return (0);
+srand(time(NULL));
+
+for (i = 0; i < 100; i++)
+{
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+if ((2772 - sum) - '0' < 78)
+{
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0');
+break;
+}
+}
+
+return (0);
 }
