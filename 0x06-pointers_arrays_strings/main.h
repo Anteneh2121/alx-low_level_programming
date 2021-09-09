530ab244bf4f4cbb64074aca2a6372ecf5386850
Anteneh <antukassaw1@gmail.com>
2021-09-09 05:36:05 -0700

    print_buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,4 +13,5 @@ char *rot13(char *);
 int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
+void print_buffer(char *b, int size);
 #endif /* MAIN_H */
