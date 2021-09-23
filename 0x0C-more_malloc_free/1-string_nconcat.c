5b6f78e173a99b896f456f59aeb646104e67cc46
Anteneh <antukassaw1@gmail.com>
2021-09-23 08:31:36 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,52 +1,51 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
+ * string_nconcat - ...
+ * @s1: ...
+ * @s2: ...
+ * @n: ...
+ *
+ * Return: ...
  */
-
-unsigned int _strlen(char *s)
-{
-unsigned int size = 0;
-for (; s[size] != '\0'; size++)
-;
-return (size);
-}
-
-/**
- * *string_nconcat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * @n: first bytes of s2 to be used
- * Return: pointer or NULL
- */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j;
-char *m;
+unsigned int i = 0, j = 0, k = 0, l = 0;
+char *str;
 
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
 
-if (n < _strlen(s2))
-m = malloc(_strlen(s1) + n * sizeof(char) + 1);
-else
-m = malloc(_strlen(s1) + _strlen(s2) + 1);
+while (s1[i])
+i++;
 
-if (m == 0)
+while (s2[k])
+k++;
+
+if (n >= k)
+l = i + k;
+else
+l = i + n;
+
+str = malloc(sizeof(char) * l + 1);
+if (str == NULL)
 return (NULL);
 
-for (i = 0; s1[i] != '\0'; i++)
-m[i] = s1[i];
+k = 0;
+while (j < l)
+{
+if (j <= i)
+str[j] = s1[j];
 
-for (j = 0; s2[j] != '\0' && j < n; i++, j++)
-m[i] = s2[j];
-
-m[i] = '\0';
-
-return (m);
+if (j >= i)
+{
+str[j] = s2[k];
+k++;
+}
+j++;
+}
+str[j] = '\0';
+return (str);
 }
