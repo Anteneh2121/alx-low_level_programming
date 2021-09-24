6eda463245a4514c2caa9a3d21b00399e4c0ae6c
Anteneh <antukassaw1@gmail.com>
2021-09-24 09:09:04 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,7 +4,6 @@
  * @s1: pointer to first string.
  * @s2: pointer to second string from which we'll take first n bytes.
  * @n: n bytes of s2.
- *
  * Return: pointer to newly allocated space in memory.
  * NULL if malloc fails.
  */
@@ -12,7 +11,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *nstr;
 unsigned int len1, len2, size, i, j;
-
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
@@ -23,11 +21,9 @@ len1++;
 len2 = 0;
 while (s2[len2] != '\0')
 len2++;
-
 if (n >= len2)
 n = len2;
 size = len1 + n;
-
 nstr = malloc((sizeof(char) * size) +1);
 if (nstr == NULL)
 return (NULL);
