fb9a7af1e17563079063a6952a42fbaac077c0ea
Anteneh <antukassaw1@gmail.com>
2021-09-24 11:12:53 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,40 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * string_nconcat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
- * @n: amount of bytes.
+ * @s1: first string to copy
+ * @s2: second string to copy
+ * @n: number of bytes of s2 to copy
  *
- * Return: pointer to the allocated memory.
- * if malloc fails, status value is equal to 98.
+ * Return: char pointer to newly allocated place in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *sout;
-unsigned int ls1, ls2, lsout, i;
-
+unsigned int i, j, k;
+char *s;
 if (s1 == NULL)
-s1 = "";
-
-if (s2 == NULL)
-s2 = "";
-
-for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-;
-
-for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-;
-
-if (n > ls2)
-n = ls2;
-
-lsout = ls1 + n;
-
-sout = malloc(lsout + 1);
-
-if (sout == NULL)
-return (NULL);
-
-for (i = 0; i < lsout; i++)
-if (i < ls1)
-sout[i] = s1[i];
+i = 0;
 else
-sout[i] = s2[i - ls1];
-
-sout[i] = '\0';
-
-return (sout);
+{
+for (i = 0; s1[i]; i++)
+;
+}
+if (s2 == NULL)
+j = 0;
+else
+{
+for (j = 0; s2[j]; j++)
+;
+}
+if (j > n)
+j = n;
+s = malloc(sizeof(char) * (i + j + 1));
+if (s == NULL)
+return (NULL);
+for (k = 0; k < i; k++)
+s[k] = s1[k];
+for (k = 0; k < j; k++)
+s[k + i] = s2[k];
+s[i + j] = '\0';
+return (s);
 }
