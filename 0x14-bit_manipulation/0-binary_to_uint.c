f08c766c29a750a2e1317db6bd551b8433688267
Anteneh Bizuneh <antukassaw1@gmail.com>
2021-10-14 02:47:51 -0700

    Bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert binary string to decimal
+ * @b: binary string
+ *
+ * Return: decimal (unsigned int)
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int decimal;
+unsigned int i;
+
+for (decimal = 0, i = 0; b[i] != '\0'; i++)
+{
+if (b[i] == '1')
+decimal = (decimal << 1) | 1;
+else if (b[i] == '0')
+decimal <<= 1;
+else if (b[i] != '0' && b[i] != '1')
+return (0);
+}
+
+return (decimal);
+}
---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_endianness - retunrs the endian of the processor
+ *
+ * Return: 1 for little and 0 for big
+ */
+
+int get_endianness(void)
+{
+unsigned int i;
+return ((int) (((char *)&i)[0]));
+}
