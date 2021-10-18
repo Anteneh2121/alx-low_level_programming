5f17bb6971e61f5e2efe94aa529687d9f7eb2620
Anteneh Bizuneh <antukassaw1@gmail.com>
2021-10-18 13:58:56 -0700

    FILE IN/OUT

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,48 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
- * read_textfile - read a file.
- * @filename: the file to read.
- * @letters: the number of chars to read
- * Description: read a file
- * section header: the header of this function is holberton.c
- * Return: this return the number of chars.
+ * read_textfile - a function that reads a text file and prints it
+ *                to POSIX standard output.
+ *
+ * @filename: is the file to read
+ * @letters: number of letters to read and print from file
+ *
+ * Return: 0 if it fails or actual number of letters it could
+ *         read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd, _read, _write;
-char *buffer;
+  int file;
+  ssize_t read_check, wcount;
+  char *buffer;
 
-if (filename == NULL)
-return (0);
+  if (filename == NULL) /*check if file is present*/
+    return (0);
 
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
-return (0);
+  file = open(filename, O_RDONLY); /*open file*/
 
-fd = open(filename, O_RDONLY, 600);
-if (fd == -1)
-{
-free(buffer);
-return (0);
-}
-_read = read(fd, buffer, letters);
-if (_read == -1)
-{
-free(buffer);
-return (0);
-}
+  if (file == -1)
+    return (0);
 
-_write = write(STDOUT_FILENO, buffer, _read);
-if (_write == -1 || _write != _read)
-{
-return (0);
-}
+  /*get the size of buffer from number of letters*/
+  buffer = malloc(sizeof(char) * letters);
+  if (buffer == NULL)
+    {
+      free(buffer);
+      return (0);
+    }
 
-free(buffer);
-close(fd);
-return (_write);
+  read_check = read(file, buffer, letters); /*read file*/
+  if (read_check == -1) /*check if read failed*/
+    return (0);
+
+  wcount = write(STDOUT_FILENO, buffer, read_check); /*write to POSIX*/
+  if (wcount == -1 || read_check != wcount) /*check if write failed*/
+    return (0);
+
+  free(buffer);
+
+  close(file); /*close file*/
+
+  return (wcount);
 }
