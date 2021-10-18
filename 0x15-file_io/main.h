b0b4763c53eb555bfcbc3e32ba064703d236c74d
Anteneh Bizuneh <antukassaw1@gmail.com>
2021-10-18 14:13:16 -0700

    File In/out

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,10 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+
+
 int _putchar(char c);
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
