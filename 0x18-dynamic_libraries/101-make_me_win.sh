2b37c44f2820865ab5b6947d5ce39a549820a256
Anteneh Bizuneh <antukassaw1@gmail.com>
2021-11-29 23:05:29 -0800

    commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/libinjection.so https://github.com/Anteneh2121/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libinjection.so 
+wget -q -o /tmp/libinjection.so https://github.com/Anteneh2121/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libinjection.so 
 export LD_PRELOAD=/tmp/libinjection.so
