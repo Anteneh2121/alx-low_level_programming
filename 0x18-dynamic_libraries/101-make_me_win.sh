f17cf62ad5e178e6f275dd8c1dfccaf69de00bf8
Anteneh Bizuneh <antukassaw1@gmail.com>
2021-12-02 05:59:46 -0800

    deleted

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -q -o /tmp/libinjection.so https://github.com/Anteneh2121/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libinjection.so 
-export LD_PRELOAD=/tmp/libinjection.so
